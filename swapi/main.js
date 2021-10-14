d9d2759de9968c04aa8f536a0e313320da5c543e
Emmanuel Izilov <em5750@gmail.com>
2021-10-13 22:45:03 -0400

    added display functionality

--- swapi/main.js
+++ swapi/main.js
@@ -1,5 +1,6 @@
 const alderaanResBtn = document.querySelector('#residents-button')
-
+const container = document.querySelector('#containers')
+const nameCont = document.querySelector('#name-container')
 const baseURL = 'https://swapi.dev/api/'
 
 
@@ -8,11 +9,13 @@ alderaanResBtn.addEventListener('click', swapiData)
 function swapiData () {
     axios.get(`${baseURL}/planets/2`)
         .then((res) => {
-            console.log(res.data.residents)
+            // console.log(res.data.residents)
             for (let i = 0; i < res.data.residents.length; i++){
                 axios.get(res.data.residents[i])
                     .then((res) => {
-                        console.log(res.data.name)
+                        const resident = document.createElement('li')
+                        resident.textContent = res.data.name
+                        nameCont.appendChild(resident)
                     })
             }
         })
